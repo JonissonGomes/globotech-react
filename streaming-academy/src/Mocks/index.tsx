90c9a462da6406c898dad2d9ff61a0e272cdaa5d
PauloMarquess <paulinho_marques2012@hotmail.com>
2022-04-06 11:10:46 -0300

    adicionado contatos na página

---
+++ streaming-academy/src/Mocks/index.tsx
@@ -0,0 +1,19 @@
+
+
+export const MockContact=[{
+  name:'João Paulo',
+  img:'https://avatars.githubusercontent.com/u/62512798?v=4',
+  link:'https://github.com/PauloMarquess'
+},{
+ name:'Jonisson',
+  img:'https://avatars.githubusercontent.com/u/42873530?v=4',
+   link:'https://github.com/JonissonGomes'
+},{
+ name:'Samanta',
+  img:'https://avatars.githubusercontent.com/u/88799970?v=4',
+   link:'https://github.com/SamantaMarry'
+},{
+ name:'Mariana',
+  img:'https://media-exp1.licdn.com/dms/image/C4D03AQGRT3YFWNLSFg/profile-displayphoto-shrink_200_200/0/1632231906506?e=1654732800&v=beta&t=90YG62ucc3MmUwjTnoiJ5jh36dPVLiwrkq4RnNWU7vc',
+   link:'https://www.linkedin.com/in/marianna-maldi-veras-77a635135/'
+}]
--- streaming-academy/src/pages/Contact/index.tsx
+++ streaming-academy/src/pages/Contact/index.tsx
@@ -1,8 +1,21 @@
 
-
+import { MockContact } from "../../Mocks";
+import './index.css'
 
 const Contact = () => {
-  return <h1>teste contatos</h1>;
-}
+  return (
+    <>
+     <h1 className="title">Nosso Time</h1>
+    <div className="container-contacts">
+     
+      {MockContact.map((contact)=>(
+        <a href={contact.link} className="contacts" target='_blank'>
+           <h3 className="contact">{contact.name}</h3> 
+          <img className="photo" src={contact.img}/>
+        </a>
+      ))}
+    </div>
+  </>
+  )}
 
 export default Contact;
